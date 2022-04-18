acd800d0148be58b454b4b290187ae2720a6f310
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-04-18 09:56:28 +0200

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_073958) do
+ActiveRecord::Schema.define(version: 2022_04_18_075456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 2022_04_18_073958) do
     t.string "instagram"
     t.string "twitter"
     t.string "linkedin"
+    t.string "phone_number"
+    t.string "country"
+    t.date "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

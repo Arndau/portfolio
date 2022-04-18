da4f8c15d2463ec40015d513857b170db6bf4089
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-04-18 10:43:10 +0200

    adding job_itle to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_081704) do
+ActiveRecord::Schema.define(version: 2022_04_18_084235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2022_04_18_081704) do
     t.string "phone_number"
     t.string "country"
     t.date "birth_date"
+    t.string "job_title"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

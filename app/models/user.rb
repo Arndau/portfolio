2e7433ef3fc0a17a61b90104d460d8d56c1018bc
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-04-18 10:02:48 +0200

    adding : has_many :projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :projects, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true

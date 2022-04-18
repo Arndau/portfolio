d33264623dd4098d80de314ddf89903d80b48691
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-04-18 10:17:23 +0200

    adding link to pojects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_080844) do
+ActiveRecord::Schema.define(version: 2022_04_18_081412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_04_18_080844) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "date_of_publication"
+    t.string "used_techno"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 

14da97f65776aabdbc9c2969aed4ca73260426f3
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-04-18 10:23:46 +0200

    ok

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,9 @@
 class Project < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, uniqueness: true
+  validates :type, presence: true
+  validates :description, length: { maximum: 140 }
+  validates :used_techno, presence: true
+  validates :link, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_081412) do
+ActiveRecord::Schema.define(version: 2022_04_18_081704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2022_04_18_081412) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "date_of_publication"
     t.string "used_techno"
+    t.string "link"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 

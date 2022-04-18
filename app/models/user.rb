680d5cec6827d43011d0a79b29fd70512ab0d249
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-04-18 09:43:00 +0200

    db:migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :description, length: { maximum: 140 }
+
 end

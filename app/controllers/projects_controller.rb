cc2174d4b7987c3719de9f79aeeb57f4342a90c9
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-04-19 15:24:23 +0200

    routes modification again

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,43 @@
 class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
 
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Restaurant.new(params[:restaurant])
+    @project.save
+    redirect_to project_path(@project)
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(params[:project])
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+
+    private
+
+    def project_params
+      params.require(:project).permit(:name, :type, :description, :date_of_publication, :used_techno, :link)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :projects
-      collection do
-        get :userprojects
-      end
+  resources :users
+  resources :projects do
+    collection do
+      get :userprojects
+    end
   end
 end

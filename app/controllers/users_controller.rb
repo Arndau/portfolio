75d94d0314555c03464cbf3aac1aac09780aea5f
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-04-19 16:04:53 +0200

    routes are ok, index ok, show ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :description, :facebook, :instagram, :twitter, :linkedin, :phone_number, :country, :birth_date, :job_title, :id)
+  end
+
 end

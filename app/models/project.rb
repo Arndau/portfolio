330f594a1f7371db4d2a3fe58a524db1fd66bd60
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-04-22 17:22:02 +0200

    debug

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,13 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
   # validates :type, presence: true
   validates :description, length: { maximum: 140 }
   # validates :used_techno, presence: true
   validates :link, presence: true, uniqueness: true
+
+  has_one_attached :image
 
   def project_image
     if image.attached?

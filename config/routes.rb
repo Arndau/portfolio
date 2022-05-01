765a138a8190c8d6db96cba9a9fc46fdbe9f5b6c
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-05-01 14:58:16 +0200

    medium was shit, go back to previous stage

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :contact, only: [:create]
 
   resources :users do
     resources :projects
   end
+  resources :contact, only: [:create]
 end

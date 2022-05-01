979b35db6ffb35229f56159cdfceada3306d726b
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-05-01 13:41:41 +0200

    adding routes conact form mail setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :contact, only: [:create]
+
   resources :users do
     resources :projects
   end
